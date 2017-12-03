72f4a327a2f0ca6357a38de9dbf59d160008e2f0
Daniel Hsing <hsing.daniel@gmail.com>
2017-12-03 21:57:22 +0800

    feat: add challenges 1 and 2, add partial solution to challenge 3
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

---
+++ 3/index.js
@@ -0,0 +1,12 @@
+// WIP
+function findSteps(n) {}
+
+function generateSpiral(n) {
+  let grid = [[1]];
+  let currRow = 0;
+  let currCol = 0;
+  for (let i = 2; i <= n; i++) {
+    if (grid.length - 1 === currRow && currCol === grid[currRow].length - 1) {
+    }
+  }
+}
